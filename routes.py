a98188ccdd0bb77fe6f50ac76a7adb6e279933bd
Kristiina Suominen <13131792+Starfire87@users.noreply.github.com>
2023-02-04 13:21:15 +0200

    Add routes for log in and register

--- routes.py
+++ routes.py
@@ -1,7 +1,33 @@
 from app import app
-import visits
 from flask import render_template
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        if player.login(username, password):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Wrong username or password.")
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    if request.method == "POST":
+        username = request.form["username"]
+        password1 = request.form["password1"]
+        password2 = request.form["password2"]
+        if password1 != password2:
+            return render_template("error.html", message="The passwords do not match.)
+        if player.register(username, password1):
+            return redirect("/")
+        else:
+            return render_template("error.html", message="Registration failed.")
