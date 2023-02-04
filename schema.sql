055673b41b2e17ac168357868a630169908dca65
Kristiina Suominen <13131792+Starfire87@users.noreply.github.com>
2023-02-04 19:21:10 +0200

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,68 @@
+CREATE TABLE player (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username VARCHAR(30) UNIQUE NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    is_admin BIT NOT NULL DEFAULT 0
+)
+
+CREATE TABLE class (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(30) NOT NULL,
+    stat_adjustment TEXT NOT NULL,
+    trauma_response TEXT NOT NULL,
+    class_skills TEXT NOT NULL
+)
+
+CREATE TABLE character (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(30) NOT NULL,
+    class_id INTEGER REFERENCES class NOT NULL,
+    player_id INTEGER REFERENCES player NOT NULL,
+    level INTEGER UNSIGNED NOT NULL,
+    strength INTEGER NOT NULL,
+    speed INTEGER NOT NULL,
+    intellect INTEGER NOT NULL,
+    combat INTEGER NOT NULL,
+    sanity INTEGER NOT NULL,
+    fear INTEGER NOT NULL,
+    body INTEGER NOT NULL,
+    max_hp INTEGER UNSIGNED NOT NULL,
+    current_hp INTEGER UNSIGNED NOT NULL,
+    min_stress INTEGER UNSIGNED NOT NULL,
+    current_stress INTEGER UNSIGNED NOT NULL,
+    description TEXT,
+    campaign_id INTEGER
+)
+
+CREATE TABLE skill (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(30) NOT NULL,
+    description TEXT NOT NULL,
+    level TEXT NOT NULL --integer/enum?
+)
+
+CREATE TABLE item (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(30) NOT NULL,
+    description TEXT NOT NULL
+)
+
+CREATE TABLE campaign (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    gamemaster_id INTEGER REFERENCES player NOT NULL
+)
+
+CREATE TABLE character_item (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    character_id INTEGER REFERENCES player NOT NULL,
+    item_id INTEGER REFERENCES item NOT NULL,
+    amount INTEGER UNSIGNED NOT NULL
+)
+
+CREATE TABLE character_skill (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    character_id INTEGER REFERENCES player NOT NULL,
+    skill_id INTEGER REFERENCES skill NOT NULL
+)
