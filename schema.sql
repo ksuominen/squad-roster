945a8298cb74f6b8ca50a58daabe17fa1e9b1a7f
Kristiina Suominen <13131792+ksuominen@users.noreply.github.com>
2023-05-06 15:46:26 +0300

    Fix sql syntax

--- schema.sql
+++ schema.sql
@@ -1,24 +1,24 @@
 CREATE TABLE player (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     username VARCHAR(30) UNIQUE NOT NULL,
     password VARCHAR(30) NOT NULL,
-    is_admin BIT NOT NULL DEFAULT 0
-)
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
+);
 
 CREATE TABLE class (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
     stat_adjustment TEXT NOT NULL,
     trauma_response TEXT NOT NULL,
     class_skills TEXT NOT NULL
-)
+);
 
 CREATE TABLE character (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
     class_id INTEGER REFERENCES class NOT NULL,
     player_id INTEGER REFERENCES player NOT NULL,
-    level INTEGER UNSIGNED NOT NULL,
+    level INTEGER NOT NULL,
     strength INTEGER NOT NULL,
     speed INTEGER NOT NULL,
     intellect INTEGER NOT NULL,
@@ -26,43 +26,43 @@ CREATE TABLE character (
     sanity INTEGER NOT NULL,
     fear INTEGER NOT NULL,
     body INTEGER NOT NULL,
-    max_hp INTEGER UNSIGNED NOT NULL,
-    current_hp INTEGER UNSIGNED NOT NULL,
-    min_stress INTEGER UNSIGNED NOT NULL,
-    current_stress INTEGER UNSIGNED NOT NULL,
+    max_hp INTEGER NOT NULL,
+    current_hp INTEGER NOT NULL,
+    min_stress INTEGER NOT NULL,
+    current_stress INTEGER NOT NULL,
     description TEXT,
     campaign_id INTEGER
-)
+);
 
 CREATE TABLE skill (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
     description TEXT NOT NULL,
     level TEXT NOT NULL --integer/enum?
-)
+);
 
 CREATE TABLE item (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
     description TEXT NOT NULL
-)
+);
 
 CREATE TABLE campaign (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     description TEXT,
     gamemaster_id INTEGER REFERENCES player NOT NULL
-)
+);
 
 CREATE TABLE character_item (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     character_id INTEGER REFERENCES player NOT NULL,
     item_id INTEGER REFERENCES item NOT NULL,
-    amount INTEGER UNSIGNED NOT NULL
-)
+    amount INTEGER NOT NULL
+);
 
 CREATE TABLE character_skill (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    id SERIAL PRIMARY KEY,
     character_id INTEGER REFERENCES player NOT NULL,
     skill_id INTEGER REFERENCES skill NOT NULL
-)
+);
